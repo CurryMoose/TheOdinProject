f5f0fc1dd89d962e9873006d46daf1f09b977ffb
CurryMoose <rishaadq@gmail.com>
2021-01-12 11:29:04 -0500

    Updating Calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,36 +1,106 @@
+    
+const numButtons = document.querySelectorAll("[data-number]");
+const opButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-clear]");
+const screen = document.querySelector("[data-screen]");
 
-function add(a, b)
+let firstOperand = "";
+let secondOperand = "";
+let operator = null;
+let result = 0;
+
+numButtons.forEach((button) =>
+  button.addEventListener("click", () => appendNumber(button.textContent))
+);
+
+opButtons.forEach((button) => 
+    button.addEventListener("click", () => setOperator(button.textContent), resetScreen()));
+
+clearButton.addEventListener("click", () => clear());
+
+equalsButton.addEventListener("click", () => equate(firstOperand, secondOperand, operator));
+
+function appendNumber(number) {
+      
+      document.getElementById("compresult").value += number.trim();
+
+
+  }
+
+function setOperator(operatorSel)
 {
-    return Number(a) + Number(b);
+    return operatorSel;
+    
 }
 
-function subtract(a, b)
+function equate(firstOperand, secondOperand, operator)
 {
-    return Number(a) - Number(b);
-}
-
-function multiply(a,b)
-{
-    return Number(a) * Number(b);
-}
-
-function divide(a,b)
-{
-    return Number(a) / Number(b);
-}
-
-
-function operate(a, b, operator)
-{
-    switch(operator)
+    if(firstOperand == "" || secondOperand == "" || setOperator(operator) == null)
     {
-        case "+":
-            return add(a,b);
-
-        case "-":
-            return subtract(a,b);
-
-        case "*":
-            return multiply(a,b);
+        alert("Invalid input!");
+        clear();
     }
+    else
+    {
+        operate(firstOperand, secondOperand, operator);
+        document.getElementById("compresult").value = result.trim();
+    }
+     
+
 }
+
+
+function clear()
+{
+    document.getElement(screen).value = "";
+    firstOperand = "";
+    secondOperand = "";
+    result = 0;
+}
+
+function resetScreen()
+{
+  document.getElement(screen).value = "";
+}
+
+
+
+
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, b, operator) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      result = add(a, b);
+    case "−":
+      result = substract(a, b);
+    case "×":
+      result = multiply(a, b);
+    case "÷":
+      if 
+        (b === 0) return null;
+      else 
+        result = divide(a, b);
+    default:
+      return null;
+  }
+}
+
